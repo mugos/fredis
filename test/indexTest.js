8b546aecc5e3453908dc2ce79962f1b076887d5a
yurifrl <yurifrl@hotmail.com>
2018-04-16 18:32:54 -0300

    WIP

---
+++ test/indexTest.js
@@ -0,0 +1,12 @@
+// Helpers
+import chai from 'chai'
+
+const expect = chai.expect
+
+// Test entry point
+describe('fRedis', function() {
+  //
+  it('works', function() {
+    expect(1).to.be.eq(1)
+  })
+})
