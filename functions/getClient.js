34d465fba6fb39f68f051afc7a00b55c7bbbcb63
yurifrl <yurifrl@hotmail.com>
2018-04-16 18:25:46 -0300

    :rocket: 2

--- functions/getClient.js
+++
@@ -1,8 +0,0 @@
-// TODO: Move to other file maybe
-// redis-mock.js
-// import redis from 'redis-mock'
-import redis from 'redis'
-
-//
-export default () => redis.createClient({ host: 'lists-db' })
-
--- functions/purgeDatabase.js
+++
@@ -1,9 +0,0 @@
-import { cond, T, equals } from 'ramda'
-import { rawFlushall } from './../functional'
-
-const err = new TypeError('Incorrect Environment')
-
-export default client => env => cond([
-  [equals("test"), env => rawFlushall()],
-  [T, () => { throw err }]
-])(env)(client)
